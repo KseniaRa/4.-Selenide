2ce1fcbb38d5ac80a88a8c8c557cf5a5ee2991a4
Ksenia <kseniarassadina@gmail.com>
2021-07-14 19:01:41 +0300

    исправление после коментария

--- src/test/java/findSelenide/SelenideTest.java
+++ src/test/java/findSelenide/SelenideTest.java
@@ -4,24 +4,24 @@ import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
-public class selenideTest {
+public class SelenideTest {
     @Test
     void  SoftAssertionsTest(){
-        // открыть страницу github.com
-        open("https://github.com");
-        // ввести selenide
-        $("[name=q]").setValue("selenide").pressEnter();
-        // выбрать первый элемент из списка
-        $$(".repo-list li").first().$("a").click();
+        // открыть страницу
+        open("https://github.com/selenide/selenide");
         // Перейти в wiki
         $(byText("Wiki")).click();
         // Перейти в раздел SoftAssertions
-        $(byText("Soft assertions")).click();
+        $(".js-wiki-more-pages-link").click();
+        $(byText("SoftAssertions")).click();
         // Проверить, что на странице есть код для тестов JUNIT5
-        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class"));
+        $("#wiki-body").shouldHave(text("Using JUnit5 extend test class:"));
+
 
     }
 }
